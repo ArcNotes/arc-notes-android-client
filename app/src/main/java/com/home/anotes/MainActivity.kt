62677b4d173c28cbddb736db8495feceeef731c6
coarsehorse <coarsehorse@home.com>
2020-11-15 19:36:57 +0200

    Now it's able to do HTTP calls

--- app/src/main/java/com/home/anotes/MainActivity.kt
+++ app/src/main/java/com/home/anotes/MainActivity.kt
@@ -1,25 +1,58 @@
 package com.home.anotes
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import okhttp3.*
+import java.io.IOException
 import java.util.concurrent.atomic.AtomicInteger
 
 class MainActivity : AppCompatActivity() {
 
     val i = AtomicInteger()
+    val okHttpClient: OkHttpClient = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<MaterialButton>(R.id.jsonButton).setOnClickListener { view ->
-            findViewById<TextView>(R.id.jsonText).setText("Changed text " + i.getAndIncrement())
+        findViewById<MaterialButton>(R.id.jsonButton).setOnClickListener { _ ->
+            grabJson()
         }
+    }
+
+    fun grabJson() {
+        val url = "http://192.168.0.104:8080/api/restore"
+        val fknToken = "Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTYwMzc0NjM1OSwiZXhwIjoxNjA2MTY1NTU5fQ.4saTXudbTFPsMHKx7pNvMsnBs-j0zuU7_DvAzjUGYE_CKgYRkFj9502u8zlfcBZh"
+
+        val request = Request.Builder()
+            .addHeader("Content-Type", "application/json")
+            .addHeader("Authorization", fknToken)
+            .url(url)
+            .build()
+
+        okHttpClient.newCall(request)
+            .enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e("okHttp", "Fuck you, API", e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+                        val json = response.body?.string()
+                        Log.i("okHttp", "Received json: $json")
+                        runOnUiThread {
+                            findViewById<TextView>(R.id.jsonText).setText(json)
+                        }
+                    }
+                }
+            });
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {

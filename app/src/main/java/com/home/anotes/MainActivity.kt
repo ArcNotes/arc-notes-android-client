06ca487a346b11b9bc299c33b603b18e487101a3
coarsehorse <coarsehorse@home.com>
2020-11-22 15:25:11 +0200

    Removed autogenerated menu

--- app/src/main/java/com/home/anotes/MainActivity.kt
+++ app/src/main/java/com/home/anotes/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.home.anotes
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
@@ -36,22 +34,6 @@ class MainActivity : AppCompatActivity() {
                     toast("Cannot restore sry :/")
                 }
             }
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 }
